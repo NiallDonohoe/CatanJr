87ddfbf5b90fa1ccd75aedbe454523599872c954
NiallDonohoe <niall.donohoe@ucdconnect.ie>
2021-12-18 18:30:31 +0000

    [21] Choose Players Through GUI

--- Catan Source Code/GUI/Controller.java
+++ Catan Source Code/GUI/Controller.java
@@ -24,6 +24,12 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import Game.GameRunner;
+import Trading.BluePlayer;
+import Trading.OrangePlayer;
+import Trading.RedPlayer;
+import Trading.WhitePlayer;
+
 public class Controller {
 
 	Button button;
@@ -43,25 +49,43 @@ public class Controller {
 	//===========================================================
 
     public void playerSelection(ActionEvent event) throws IOException {
-    	String numPlayers = ((Button) event.getSource()).getText();
-    	System.out.println("Picked "+numPlayers);
-    	// Add in code to set number of players.
-		Parent ChooseColour = FXMLLoader.load(getClass().getResource("ChooseColour.fxml"));
-		Scene ChooseColourScene = new Scene(ChooseColour);
-		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		stage.setScene(ChooseColourScene);
-		stage.show();
+    	int numPlayers = (Integer.parseInt(((Button) event.getSource()).getText()));
+    	System.out.println("Picked "+numPlayers+ " Players");
+    	Game.GameRunner.setNumPlayers(numPlayers);
+		this.handleChooseColour(event);
     }
+    
+	//===========================================================
+	// Handle Colour Selection
+	//===========================================================
+    public void handleChooseColour(ActionEvent event) throws IOException {
+    	if(Game.GameRunner.players.size()< Game.GameRunner.getNumPlayers()) {
+    		Parent ChooseColour = FXMLLoader.load(getClass().getResource("ChooseColour.fxml"));
+    		Scene ChooseColourScene = new Scene(ChooseColour);
+    		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    		stage.setScene(ChooseColourScene);
+    		stage.show();
+    	}
+    	else
+    		loadMap(event);
+    		
+    }
+    
 	//===========================================================
 	// Player Colour Selection
 	//===========================================================    
     public void chooseColour(ActionEvent event) throws IOException {
     	String colour = ((Button) event.getSource()).getText();
-		System.out.println("Picked "+colour);
-
-		
-    	loadMap(event);
+    	System.out.println("Picked "+colour);
+    	
+    	if (colour.contentEquals("Blue")) { BluePlayer.getInstance(); Game.GameRunner.players.add(BluePlayer.getInstance());}
+    	if (colour.contentEquals("White")) { WhitePlayer.getInstance(); Game.GameRunner.players.add(WhitePlayer.getInstance());}
+    	if (colour.contentEquals("Red")) { RedPlayer.getInstance(); Game.GameRunner.players.add(RedPlayer.getInstance());}
+    	if (colour.contentEquals("Orange")) { OrangePlayer.getInstance(); Game.GameRunner.players.add(OrangePlayer.getInstance());}
+    	
+    	this.handleChooseColour(event);	
 	}
+    
 	//===========================================================
 	// Main Scene with map
 	//===========================================================     
